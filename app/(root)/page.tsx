09a18530d70a291720aa8c985514709336dd77c9
adnanalemran <adnanalemranontor@gmail.com>
2024-09-26 01:46:05 +0600

    Refactor code and fix file paths

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-
+      Hom
     </div>
   )
 }
 
-export default Hon
+export default Home
---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const transformationsPage = () => {
+    return (
+        <div>
+            transformationsPage
+        </div>
+    );
+};
+
+export default transformationsPage;
+
