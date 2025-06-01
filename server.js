df4be201026c0f7faf79fceff7e8bb1ad6a2ec78
adnanalemran <adnanalemranontor@gmail.com>
2025-06-02 02:26:05 +0600

    express.static

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require("express");
-
 const app = express();
 
 app.use(express.json());
-
+app.use(express.static(__dirname + "/public"));
 
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
