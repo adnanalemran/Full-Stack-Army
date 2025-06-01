da9ef096e6a42508b04ef6186417ca3e5435771b
adnanalemran <adnanalemranontor@gmail.com>
2025-06-02 00:41:33 +0600

    Refactor logging middleware for clarity and consistency in response handling

--- server.js
+++ server.js
@@ -4,24 +4,34 @@ const app = express();
 
 app.use(express.json());
 
+
 app.listen(8000, () => {
   console.log("Server is running on port 8000");
 });
 
+// Middleware to log request details and respond to a specific query parameter
 const sipleLogger = (req, res, next) => {
   console.log(`${req.method} -- ${req.url} -- ${new Date().toISOString()}`);
-  if (req.query.name === "ontor"){
+  if (req.query.name === "ontor") {
     return res.json({
       message: "Hello Ontor!",
     });
   }
- 
   next();
 };
+// Use the middleware globally
+app.use(sipleLogger);
 
-app.get("/hello",sipleLogger, (req, res, next) => {
-  
-  res.json({
+// Route that uses the middleware
+app.get("/hello", sipleLogger, (req, res, next) => {
+  return res.json({
     message: "Hello, World!",
+  });
+});
+
+// Route that does not use the middleware
+app.get("/goodbye", (req, res) => {
+  return res.json({
+    message: "Goodbye, World!",
   });
 });
